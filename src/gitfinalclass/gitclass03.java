ae1bcd5cf71313e0ff9c7e773cbde7f70153dcf8
Gul9729 <gulmanat.doszhanova@gmail.com>
2021-08-17 22:23:00 +0600

    the second commit

--- src/gitfinalclass/gitclass03.java
+++ src/gitfinalclass/gitclass03.java
@@ -3,7 +3,7 @@ package gitfinalclass;
 public class gitclass03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("This is the first commit file");
 
 	}
 
---
+++ src/gitfinalclass/gitclass03file2.java
@@ -0,0 +1,8 @@
+package gitfinalclass;
+
+public class gitclass03file2 {
+	public static void main(String[] args) {
+		System.out.println("This is the second commit file");
+	}
+
+}
